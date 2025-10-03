9c171f55683c813cb8b30db8c0245372054452f8
SalomandraC <kazak.petrushin@yandex.ru>
2025-10-03 20:37:07 +0300

    first commit

---
+++ sketch_oct3g/sketch_oct3g.ino
@@ -0,0 +1,26 @@
+#include <iarduino_RF433_Receiver.h>
+
+iarduino_RF433_Receiver radioRX(3);     // Приемник на пине 3
+
+void setup() {
+  Serial.begin(9600);
+  
+  // Инициализация приемника
+  radioRX.begin(1000);                   // Скорость 1 кбит/сек
+  radioRX.openReadingPipe(5);            // Труба №5
+  radioRX.startListening();
+  
+  Serial.println("Arduino Uno Receiver ready");
+  Serial.println("Waiting for numbers from ESP32...");
+}
+
+void loop() {
+  // Простая версия - принимаем строку
+  if (radioRX.available()) {
+    char receivedData[10] = "";
+    radioRX.read(&receivedData, sizeof(receivedData));
+    
+    Serial.print("Received: ");
+    Serial.println(receivedData);
+  }
+}
