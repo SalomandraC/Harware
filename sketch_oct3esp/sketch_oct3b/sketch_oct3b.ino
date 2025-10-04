989619dd39fb736b45081ffb7c945fc7402047a8
SalomandraC <kazak.petrushin@yandex.ru>
2025-10-04 18:22:47 +0300

    esp32 full

--- sketch_oct3esp/sketch_oct3b/sketch_oct3b.ino
+++ sketch_oct3esp/sketch_oct3b/sketch_oct3b.ino
@@ -1,49 +1,27 @@
-#include <RCSwitch.h>
+// ESP32_transmitter_RH_ASK.ino
+#include <RH_ASK.h>
+#include <SPI.h> 
 
-RCSwitch mySwitch = RCSwitch();
+RH_ASK rfdriver(2000, 0, 2, 0);
 
 void setup() {
   Serial.begin(115200);
-  
-  // Передатчик на пине 4
-  mySwitch.enableTransmit(2);
-  // Приемник на пине 5
-  mySwitch.enableReceive(4);
-
-  mySwitch.setProtocol(1);          // Протокол 1
-  mySwitch.setPulseLength(300);     // Длина импульса
-  mySwitch.setRepeatTransmit(5);    // Повторов для надежности
-  
-  Serial.println("ESP32 Ready - Communicating with Arduino Uno");
+  delay(100);
+  if (!rfdriver.init()) {
+    Serial.println("RF init failed");
+  } else {
+    Serial.println("RF init OK");
+  }
 }
 
 void loop() {
-  static unsigned long lastSend = 0;
-  if (millis() - lastSend > 3000) {
-    static int counter = 1000;
-    mySwitch.send(counter, 24);     
-    
-    Serial.print("ESP32 → Arduino: ");
-    Serial.println(counter);
-    
-    counter++;
-    if (counter > 1010) counter = 1000;
-    lastSend = millis();
-  }
+  unsigned long number = 12345;
+  char buf[16];
+  snprintf(buf, sizeof(buf), "%lu", number);
+  rfdriver.send((uint8_t*)buf, strlen(buf));
+  rfdriver.waitPacketSent();
 
-  if (mySwitch.available()) {
-    unsigned long receivedValue = mySwitch.getReceivedValue();
-    
-    if (receivedValue != 0) {
-      Serial.print("ESP32 ← Arduino: ");
-      Serial.println(receivedValue);
-      if (receivedValue == 123456) {
-        Serial.println("  → Special command: HELLO");
-      }
-    }
-    
-    mySwitch.resetAvailable();
-  }
-  
-  delay(100);
+  Serial.print("Sent: ");
+  Serial.println(buf);
+  delay(1000);
 }
