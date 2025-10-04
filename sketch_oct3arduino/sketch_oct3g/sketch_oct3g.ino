4ff6ca5737d56c9ab7aa25f808f07d0d29c9d8e5
SalomandraC <kazak.petrushin@yandex.ru>
2025-10-04 16:49:31 +0300

    Radio Head

--- sketch_oct3arduino/sketch_oct3g/sketch_oct3g.ino
+++ sketch_oct3arduino/sketch_oct3g/sketch_oct3g.ino
@@ -1,59 +1,25 @@
-#include <iarduino_RF433_Transmitter.h>
-#include <iarduino_RF433_Receiver.h>
+// Arduino_receiver_RH_ASK.ino
+#include <RH_ASK.h>
+#include <SPI.h>
 
-iarduino_RF433_Transmitter radioTX(5);
-iarduino_RF433_Receiver radioRX(3);
-
-// Буферы для данных
-uint32_t txData = 0;
-uint32_t rxData = 0;
+RH_ASK rfdriver(2000, 3, 0, 0);
 
 void setup() {
   Serial.begin(9600);
-  
-  // Инициализация RF433
-  radioTX.begin(1000);      // Скорость 1000 бит/с
-  radioRX.begin(1000);
-  
-  radioTX.openWritingPipe(5);   // Труба 5
-  radioRX.openReadingPipe(6);   // Труба 5
-  radioRX.startListening();
-  
-  Serial.println("Arduino Uno Ready - Communicating with ESP32");
+  if (!rfdriver.init()) {
+    Serial.println("RF init failed");
+  } else {
+    Serial.println("RF init OK");
+  }
 }
 
 void loop() {
-  // ПРИЕМ ДАННЫХ ОТ ESP32
-  uint8_t pipeNum;
-  if (radioRX.available(&pipeNum)) {
-    // Читаем данные как число (совместимость с RCSwitch)
-    radioRX.read(&rxData, sizeof(rxData));
-    
-    Serial.print("Arduino ← ESP32: ");
-    Serial.println(rxData);
-    
-    // Реагируем на полученные данные
-    if (rxData >= 1000 && rxData <= 1010) {
-      Serial.println("  → Counter received from ESP32");
-    }
+  uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+  uint8_t buflen = sizeof(buf);
+
+  if (rfdriver.recv(buf, &buflen)) { // блокирует короткий момент при приеме
+    buf[buflen] = 0; // терминируем строку
+    Serial.print("Received: ");
+    Serial.println((char*)buf);
   }
-  
-  // ПЕРЕДАЧА ДАННЫХ ДЛЯ ESP32
-  static unsigned long lastSend = 0;
-  if (millis() - lastSend > 4000) {
-    // Отправляем числа для совместимости с RCSwitch
-    static int arduinoCounter = 5000;
-    txData = arduinoCounter;
-    
-    radioTX.write(&txData, sizeof(txData));
-    
-    Serial.print("Arduino → ESP32: ");
-    Serial.println(arduinoCounter);
-    
-    arduinoCounter++;
-    if (arduinoCounter > 5010) arduinoCounter = 5000;
-    lastSend = millis();
-  }
-  
-  delay(200);
 }
